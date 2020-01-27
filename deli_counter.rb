b5f86821511f4c250517f56ae5aad460340c74da
Leonor Colbert <leonoracolbert@gmail.com>
2020-01-27 19:55:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,9 @@ def line(katz_deli)
       puts "The line is currently empty."
     else
       line_count = "The line is currently:"
+      customer_list = []
       katz_deli.each_with_index |name, i|
+       customer_list << ""
       
       
     end
