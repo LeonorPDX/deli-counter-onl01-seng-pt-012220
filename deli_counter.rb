7fc11cc8f81bbb96a4061473166d60524b9aa162
Leonor Colbert <leonoracolbert@gmail.com>
2020-01-27 20:14:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,12 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    next_customer = katz_deli.shift
+    puts "Now serving #{next_customer}."
+  end
+end
