a91a3f01fd7b26ae7d776f9688c139b2df55b085
Leonor Colbert <leonoracolbert@gmail.com>
2020-01-27 19:57:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(katz_deli)
     if katz_deli.length == 0
       puts "The line is currently empty."
     else
-      line_count = "The line is currently:"
+      line_count = "The line is currently: "
       katz_deli.each_with_index |name, i|
-       line_count << "#{i+1}"
+       line_count << "#{i+1}. #{name} "
       
       
     end
